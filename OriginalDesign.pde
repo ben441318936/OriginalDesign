226f9cfe6adce854d19c26dd528c5f03086e483a
unknown <jihuang65@697-334-Andunie.sf.k12.internal>
2014-08-27 10:31:59 -0700

    Created an original design.

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,55 @@
+/*
+Jingbin Huang
+Block 4
+Ap Computer Science
+*/
+int waveWidth = 0;
+int waveHeight = 0;
+int counter = 0;
 void setup()
 {
-
+	size(500,500);
+	background(0);
+	cellTower();
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+	frameRate(2);
+	wave();
+	if (waveWidth==300)
+	{
+		background(0);
+		cellTower();
+		waveWidth=0;
+		waveHeight=0;
+	}
+	counter++;
+	if (counter == 10)
+	{
+
+	}
 }
 
+void cellTower()
+{
+	noFill();
+	strokeWeight(2);
+	stroke(250,50,50);
+	beginShape();
+	vertex(200,450);
+	vertex(250,200);
+	vertex(300,450);
+	vertex(250,400);
+	vertex(200,450);
+	endShape();
+}
+
+void wave()
+{
+	noFill();
+	strokeWeight(2);
+	stroke(90, 200, 250);
+	ellipse(250, 200, waveWidth, waveHeight);
+	waveWidth=waveWidth+30;
+	waveHeight=waveHeight+30;
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,56 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int waveWidth = 0;
+int waveHeight = 0;
+int counter = 0;
 public void setup()
 {
-
+	size(500,500);
+	background(0);
+	cellTower();
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+	frameRate(2);
+	wave();
+	if (waveWidth==300)
+	{
+		background(0);
+		cellTower();
+		waveWidth=0;
+		waveHeight=0;
+	}
+	counter++;
+	if (counter == 10)
+	{
+		
+	}
 }
 
+public void cellTower()
+{
+	noFill();
+	strokeWeight(2);
+	stroke(250,50,50);
+	beginShape();
+	vertex(200,450);
+	vertex(250,200);
+	vertex(300,450);
+	vertex(250,400);
+	vertex(200,450);
+	endShape();
+}
+
+public void wave()
+{
+	noFill();
+	strokeWeight(2);
+	stroke(90, 200, 250);
+	ellipse(250, 200, waveWidth, waveHeight);
+	waveWidth=waveWidth+30;
+	waveHeight=waveHeight+30;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
